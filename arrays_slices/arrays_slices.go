901c040a0c138b5f400fe9ca479329dd293c0355
diendang <diendang@chotot.vn>
2021-06-26 16:31:39 +0700

    test sum of slices - go test coverage 100%

--- arrays_slices/arrays_slices.go
+++ arrays_slices/arrays_slices.go
@@ -1,9 +1,17 @@
 package arrays_slices
 
-func Sum(numbers [5]int) int {
-	var sum = 0
+func Sum(numbers [5]int) (sum int) {
 	for _, number := range numbers {
 		sum += number
 	}
-	return sum
+	return
 }
+
+func SumV2(numbers []int) (sum int) {
+	for _, number := range numbers {
+		sum += number
+	}
+	return
+}
+
+
--- arrays_slices/arrays_slices_test.go
+++ arrays_slices/arrays_slices_test.go
@@ -11,3 +11,16 @@ func TestSum(t *testing.T) {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
 	}
 }
+
+func TestSumV2(t *testing.T)  {
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := SumV2(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
