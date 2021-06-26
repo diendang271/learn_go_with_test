3117fd16a4c6fe0a374f00b219a230548ae1c202
diendang <diendang@chotot.vn>
2021-06-26 16:08:03 +0700

    add example Repeat()

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,18 +1,37 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
-
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+	assertionRepeatMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("expected %q but got %q", want, got)
+		}
 	}
+	t.Run("test repeated 5 times", func(t *testing.T) {
+		got := Repeat("A", 5)
+		want := "AAAAA"
+		assertionRepeatMessage(t, got, want)
+	})
+	t.Run("test repeated 0 times", func(t *testing.T) {
+		got := Repeat("A", 0)
+		want := ""
+		assertionRepeatMessage(t, got, want)
+	})
+}
+
+func ExampleRepeat()  {
+	result := Repeat("A", 5)
+	fmt.Println(result)
+	// Output: AAAAA
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
