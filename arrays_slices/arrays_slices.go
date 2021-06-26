edd8f46b0ce3262a083103969dffa355d982b2a0
diendang <diendang@chotot.vn>
2021-06-26 22:38:49 +0700

    another test of slices - sumAllTail

--- arrays_slices/arrays_slices.go
+++ arrays_slices/arrays_slices.go
@@ -14,4 +14,23 @@ func SumV2(numbers []int) (sum int) {
 	return
 }
 
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, SumV2(numbers))
+	}
+	return
+}
 
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, SumV2(tail))
+		}
+	}
+	return sums
+}
