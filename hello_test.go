991c795273d474e1d31554238d83af19bfeda37c
diendang <diendang@chotot.vn>
2021-06-26 14:26:18 +0700

    refactor: use t.Helper in test, use t.Run for subtest

--- hello_test.go
+++ hello_test.go
@@ -2,11 +2,39 @@ package main
 
 import "testing"
 
+// Test function name should be start with "Test" keyword
+// It only takes one arguments *testing.T
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		// t.Helper() is needed to tell the test suite that this method is a helper.
+		// By doing this when it fails the line number reported will be in our function call
+		// rather than inside our test helper. This will help other developers track down
+		// problems easier.
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	// Here we are introducing another tool in our testing arsenal,
+	//subtests. Sometimes it is useful to group tests around a "thing"
+	//and then have subtests describing different scenarios.
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		if got != want {
+			// t.Errorf() print error to stdout and fail the test.
+			assertCorrectMessage(t, got, want)
+		}
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		if got != want {
+			assertCorrectMessage(t, got, want)
+		}
+
+	})
+
 }
